683f4d05e45ca58787036d6f02d428e51ea31176
Mohammad Pezeshki <mohammadpz@gmail.com>
2022-12-07 13:42:29 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # pytorch_forward_forward
-Implementation of Hinton's forward-forward (FF) algorithm - an alternative to back-propagation 
+Implementation of forward-forward (FF) training algorithm - an alternative to back-propagation
+---
+
+Below is my understanding of the FF algorithm presented at [Geoffrey Hinton's talk at NeurIPS 2022](https://www.cs.toronto.edu/~hinton/FFA13.pdf).
+The conventional backprop computes the gradients by successive applications of the chain rule, from the objective function to the parameters. FF, however, computes the gradients locally with a local objective function, so there is no need to backpropagate the errors.
+
+![](./imgs/BP_vs_FF.png)
+
+The local objective function is designed to push a layer's output to values larger than a threshold for positive samples and to values smaller than a threshold for negative samples.
+
+A positive sample $s$ is a real datapoint with a large $P(s)$ under the training distribution.
+A negative sample $s'$ is a fake datapoint with a small $P(s')$ under the training distribution.
+
+![](./imgs/layer.png)
+
+Among the many ways of generating the positive/negative samples, for MNIST, we have:
+Positive sample $s = merge(x, y)$, the image and its label
+Negative sample $s' = merge(x, y_rand)$, the image and a random label
+
+![](./imgs/pos_neg.png)
+
+After training all the layers, to make a prediction for a test image $x$, we find the pair $s = (x, y)$ for all $0 \leq y < 10$ that maximizes the network's overall activation.
+
+With this implementation, the training and test accuracy on MNIST are:
+```python
+> python main.py
+train error: 0.06520003080368042
+test error: 0.09890002012252808
+```
---
+++ main.py
@@ -0,0 +1,114 @@
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+from torch.optim import Adam
+from torchvision.datasets import MNIST
+from torchvision.transforms import Compose, ToTensor, Normalize, Lambda
+from torch.utils.data import DataLoader
+
+
+def MNIST_loaders(train_batch_size=50000, test_batch_size=10000):
+
+    transform = Compose([
+        ToTensor(),
+        Normalize((0.1307,), (0.3081,)),
+        Lambda(lambda x: torch.flatten(x))])
+
+    train_loader = DataLoader(
+        MNIST('./data/', train=True,
+              download=True,
+              transform=transform),
+        batch_size=5000, shuffle=True)
+
+    test_loader = DataLoader(
+        MNIST('./data/', train=False,
+              download=True,
+              transform=transform),
+        batch_size=10000, shuffle=False)
+
+    return train_loader, test_loader
+
+
+def overlay_y_on_x(x, y):
+    x_ = x.clone()
+    x_[:, :10] *= 0.0
+    x_[range(x.shape[0]), y] = x.max()
+    return x_
+
+
+class Net(torch.nn.Module):
+
+    def __init__(self, dims):
+        super().__init__()
+        self.layers = []
+        for d in range(len(dims) - 1):
+            self.layers += [Layer(dims[d], dims[d + 1]).cuda()]
+
+    def predict(self, x):
+        goodness_per_label = []
+        for label in range(10):
+            h = overlay_y_on_x(x, label)
+            goodness = []
+            for layer in self.layers:
+                h = layer(h)
+                goodness += [h.pow(2).mean(1)]
+            goodness_per_label += [sum(goodness).unsqueeze(1)]
+        goodness_per_label = torch.cat(goodness_per_label, 1)
+        return goodness_per_label.argmax(1)
+
+    def train(self, x_pos, x_neg):
+        h_pos, h_neg = x_pos, x_neg
+        for i, layer in enumerate(self.layers):
+            print('training layer', i, '...')
+            h_pos, h_neg = layer.train(h_pos, h_neg)
+
+
+class Layer(nn.Linear):
+    def __init__(self, in_features, out_features,
+                 bias=True, device=None, dtype=None):
+        super().__init__(in_features, out_features, bias, device, dtype)
+        self.relu = torch.nn.ReLU()
+        self.opt = Adam(self.parameters(), lr=0.03)
+        self.threshold = 2.0
+        self.num_epochs = 1000
+
+    def forward(self, x):
+        x_direction = x / (x.norm(2, 1, keepdim=True) + 1e-4)
+        return self.relu(
+            torch.mm(x_direction, self.weight.T) +
+            self.bias.unsqueeze(0))
+
+    def train(self, x_pos, x_neg):
+        for i in tqdm(range(self.num_epochs)):
+            g_pos = self.forward(x_pos).pow(2).mean(1)
+            g_neg = self.forward(x_neg).pow(2).mean(1)
+            # The following loss pushes pos (neg) samples to
+            # values larger (smaller) than the self.threshold.
+            loss = torch.log(1 + torch.exp(torch.cat([
+                -g_pos + self.threshold,
+                g_neg - self.threshold]))).mean()
+            self.opt.zero_grad()
+            # this backward just compute the derivative and hence
+            # is not considered backpropagation.
+            loss.backward()
+            self.opt.step()
+        return self.forward(x_pos).detach(), self.forward(x_neg).detach()
+
+if __name__ == "__main__":
+    torch.manual_seed(1234)
+    train_loader, test_loader = MNIST_loaders()
+
+    net = Net([784, 500, 500])
+    x, y = iter(train_loader).next()
+    x, y = x.cuda(), y.cuda()
+    x_pos = overlay_y_on_x(x, y)
+    rnd = torch.randperm(x.size(0))
+    x_neg = overlay_y_on_x(x, y[rnd])
+    net.train(x_pos, x_neg)
+
+    print('train error:', 1.0 - net.predict(x).eq(y).float().mean().item())
+
+    x_te, y_te = iter(test_loader).next()
+    x_te, y_te = x_te.cuda(), y_te.cuda()
+
+    print('test error:', 1.0 - net.predict(x_te).eq(y_te).float().mean().item())
